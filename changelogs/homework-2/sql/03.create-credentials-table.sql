2b6beb823736f34824c61113b36d45b07341fb2f
cryptosuka123 <goldsashko@gmail.com>
2021-11-06 00:11:55 +0100

    restructuring

--- changelogs/homework-2/sql/03.create-credentials-table.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS credentials;
-CREATE TABLE credentials
-(
- id serial  PRIMARY KEY ,
- password    varchar(50) NOT NULL ,
- login        varchar(50) NOT NULL );
--- changelogs/homework-2/sql/08.create-index.sql
+++
@@ -1,8 +0,0 @@
-
-Create index prodConfId on productConfig(productId);
-Create index categoryId on products(categoryId);
-create index userId on products(userId);
-create index histProductId on history(productId);
-create index histCustomerId on history(customerId);
-create index credentialsID on users(credentialsId);
-create index roleId on users(roleId);
